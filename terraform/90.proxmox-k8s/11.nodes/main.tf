a9a2e0fdd1163829b95f3eeee53ae56761fc9cb3
fudoge <77766067+fudoge@users.noreply.github.com>
2026-05-31 18:58:49 +0900

    fix: scale up worker-1 node

--- terraform/90.proxmox-k8s/11.nodes/main.tf
+++ terraform/90.proxmox-k8s/11.nodes/main.tf
@@ -33,8 +33,8 @@ locals {
     "worker-1" : {
       vm_name   = "worker-1"
       vm_id     = "1100",
-      cpu_cores = 2
-      memory    = 4096
+      cpu_cores = 4
+      memory    = 8192
       networks = [
         { bridge = "vmbr1", ip = "192.168.10.100/24", gw = local.frroute_ip }
       ]
